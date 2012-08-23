fc0825edec385a0410ad334d17cf5cf6aa0edd72
Mike Spainhower <mcspainhower@gmail.com>
2012-08-23 03:16:13 -0400

    Added config and helper for login link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def oauth_url()
+    oauth_info = Rails.configuration.oauth_info
+    url = oauth_info[:oauth_url]
+    url << "?client_id=#{oauth_info[:client_id]}"
+    url << "&response_type=#{oauth_info[:response_type]}"
+    url << "&redirect_uri=#{request.protocol}#{request.host_with_port}#{request.fullpath}"
+    url << "&scope=#{oauth_info[:scope]}"
+    url << "&update=#{oauth_info[:update]}"
+    return URI::encode url
+  end
 end
---
+++ config/initializers/oauth.rb
@@ -0,0 +1,9 @@
+Mynotes::Application.config.oauth_info = {
+  :oauth_url => "https://api-sandbox.personal.com/oauth/authorize",
+  :client_id => "gb4gjczj2nj7gjtq545bnzv3",
+  :response_type => "code",
+  :scope => "read_0135,write_0135,create_0135",
+  :update => "true",
+  :grant_type => "authorization_code",
+  :client_secret => "caXZdaNE7xUHE8spT53K27uT"
+}
