9c0fcf59527c170de292a9f8d7a26ee4cb1b062e
Mike Spainhower <mcspainhower@gmail.com>
2012-09-04 17:17:06 -0400

    Updated login link helper so that 'logout' doesn't end up in query string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     url = "#{oauth_info[:oauth_url]}/authorize"
     url << "?client_id=#{oauth_info[:client_id]}"
     url << "&response_type=#{oauth_info[:response_type]}"
-    url << "&redirect_uri=#{request.protocol}#{request.host_with_port}#{request.fullpath}"
+    url << "&redirect_uri=#{request.protocol}#{request.host_with_port}#{request.path}"
     url << "&scope=#{oauth_info[:scope]}"
     url << "&update=#{oauth_info[:update]}"
     return URI::encode url
