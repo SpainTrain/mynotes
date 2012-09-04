0db938279332bba805530954b96ab6ceb16e8606
Mike Spainhower <mcspainhower@gmail.com>
2012-09-04 17:15:59 -0400

    Added API url to config

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -1,9 +1,16 @@
 Mynotes::Application.config.oauth_info = {
   :oauth_url => "https://api-sandbox.personal.com/oauth",
+  :api_url => "https://api-sandbox.personal.com/api/v1",
   :client_id => "gb4gjczj2nj7gjtq545bnzv3",
   :response_type => "code",
   :scope => "read_0135,write_0135,create_0135",
   :update => "true",
   :grant_type => "authorization_code",
   :client_secret => "caXZdaNE7xUHE8spT53K27uT"
+}
+
+Mynotes::Application.config.ssl_info ={
+  :ca_path => '/etc/ssl/certs',
+  :ca_file => '/etc/ssl/certs/ca-certificates.crt',
+  :verify_mode => OpenSSL::SSL::VERIFY_NONE
 }
